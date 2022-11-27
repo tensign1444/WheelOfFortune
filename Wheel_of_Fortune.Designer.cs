d5eb8e9b070ec8d5035ad19de9b2d9a9e4839a42
kmurph27 <115516133+kmurph27@users.noreply.github.com>
2022-11-27 06:47:28 -0800

    Add files via upload

---
+++ Wheel_of_Fortune.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Wheel_of_Fortune_Wheel
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WOF_Wheel = new System.Windows.Forms.PictureBox();
+            this.Spin_Button = new System.Windows.Forms.Button();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.WOF_Wheel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WOF_Wheel
+            // 
+            this.WOF_Wheel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WOF_Wheel.BackColor = System.Drawing.Color.White;
+            this.WOF_Wheel.BackgroundImage = global::Wheel_of_Fortune_Wheel.Properties.Resources.No_slots_active;
+            this.WOF_Wheel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.WOF_Wheel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.WOF_Wheel.Location = new System.Drawing.Point(91, 104);
+            this.WOF_Wheel.Name = "WOF_Wheel";
+            this.WOF_Wheel.Size = new System.Drawing.Size(512, 467);
+            this.WOF_Wheel.TabIndex = 0;
+            this.WOF_Wheel.TabStop = false;
+            // 
+            // Spin_Button
+            // 
+            this.Spin_Button.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Spin_Button.BackColor = System.Drawing.Color.Gold;
+            this.Spin_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Spin_Button.Font = new System.Drawing.Font("Broadway", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Spin_Button.Location = new System.Drawing.Point(230, 577);
+            this.Spin_Button.Name = "Spin_Button";
+            this.Spin_Button.Size = new System.Drawing.Size(234, 71);
+            this.Spin_Button.TabIndex = 1;
+            this.Spin_Button.Text = "Spin That Wheel!";
+            this.Spin_Button.UseVisualStyleBackColor = false;
+            this.Spin_Button.Click += new System.EventHandler(this.Spin_Button_Click);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Broadway", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(186, 36);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(322, 36);
+            this.TitleLabel.TabIndex = 2;
+            this.TitleLabel.Text = "Wheel of Fortune";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 659);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.Spin_Button);
+            this.Controls.Add(this.WOF_Wheel);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Wheel_of_Fortune";
+            this.Click += new System.EventHandler(this.Spin_Button_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.WOF_Wheel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Spin_Button;
+        private System.Windows.Forms.PictureBox WOF_Wheel;
+        private System.Windows.Forms.Label TitleLabel;
+    }
+}
+
---
+++ Wheel_of_Fortune.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wheel_of_Fortune_Wheel
+{
+    public partial class Main : Form
+    {
+        //Allows the use of the current working directory where the images are then retrieved
+        static string livePath = Environment.CurrentDirectory;
+        //Utilized in order to create a class of images to use for the spin.
+        static Image[] images = new Image[24];
+        //Sets the current slot number to 0.
+        static int i = 0;
+        //Used to make the spinner start in the non spinning
+        //position then trigger once button is pressed.
+        static bool spinning = false;
+        //Used to control the SpinTheWheel function below.
+        static Thread SpinWheel = new Thread(SpinTheWheel);
+        //Used to close and open SpinWheel throughtout the code.
+        static PictureBox wheel = new PictureBox();
+        //Initializes the main program and assigns WOF_Wheel to wheel.
+        public Main()
+        {
+            InitializeComponent();
+            wheel = WOF_Wheel;
+        }
+        //Allows the the looping through of all png images, which
+        //is what makes it look like the wheel is spinning.
+        static void ImageChanger()
+        {
+            if (i + 1 >= images.Length)
+                i = 0;
+            else
+                i++;
+            //Collects the images from the current resource directory.
+            images[i] = Image.FromFile(livePath + "\\Resources\\Slot " + (i + 1) + " active.png");
+            wheel.Invoke(new Action(() => wheel.BackgroundImage = images[i]));
+        }
+        //Supposed to spin the wheel at random but it is currently not doing so
+        //and keeps landing on the same one.
+        static void SpinTheWheel()
+        {
+            Random random = new Random();
+            int cycle = 1;
+
+            for (int y = 0; y < 5; y++)
+            {
+                //spin speed
+                int spin = random.Next((400 - ((cycle - 1) * 150)), (460 - ((cycle - 1) * 110)));
+                for (int x = 0; x < spin; x++)
+                {
+                    ImageChanger();
+                    Thread.Sleep(10 * cycle);
+                }
+                cycle++;
+            }
+            //This is also have troubles working as it will not actually print the wedge that was landed on.
+            spinning = false;
+            MessageBox.Show("Congrats!! You won prize " + (i + 1));
+            SpinWheel.Abort();
+        }
+        //Code to make the wheel spin upon click event.
+        private void Spin_Button_Click(object sender, EventArgs e)
+        {
+            if (!spinning)
+            {
+                Thread SpinWheel = new Thread(SpinTheWheel);
+                spinning = true;
+                SpinWheel.Start();
+            }
+        }
+    }
+}
